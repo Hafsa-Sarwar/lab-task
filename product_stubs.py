2025fb426de0a48447f2eb595e1c64d202b4ec4d
hafsa-sarwar <hafsasarwar190@gmail.com>
2024-09-27 22:10:09 +0500

    added get price method

---
+++ product_stubs.py
@@ -0,0 +1,32 @@
+class Product:
+  def _init_(self, name, amount, price):
+      self.name = name
+      self.amount = amount
+      self.price = price
+
+  def get_price(self, quantity):
+    try:
+        if quantity <= 0:
+            raise ValueError("Quantity can't be less than 1.")
+        
+        if quantity < 10:
+            discount_amount = 0
+        elif 10 <= quantity < 100:
+            discount_amount = actual_price * 0.10 
+        else:
+            discount_amount = actual_price * 0.20  
+        
+        final_price = actual_price - discount_amount
+        
+        return final_price
+
+    except ValueError as e:
+        print("Price calculation failed:", e)
+        return None  
+    # any other exception
+    except Exception as e:
+        print("Price calculation failed:", e)
+        return None
+
+  def make_purchase(self, quantity):
+      pass
