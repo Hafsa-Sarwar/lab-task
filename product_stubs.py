812021745aa0fd1ad45fd3f8b32f3ba35f8824b5
hafsa-sarwar <hafsasarwar190@gmail.com>
2024-09-27 22:51:02 +0500

    added the tasks for main file

--- product_stubs.py
+++ product_stubs.py
@@ -30,3 +30,15 @@ class Product:
 
   def make_purchase(self, quantity):
       pass
+
+product = Product("Laptop", 20, 50000)
+
+product.make_purchase(100)
+product.make_purchase(-5)
+product.make_purchase(0)
+
+product1 = Product("Watch", 10, 2000)
+
+product1.make_purchase(100)
+product1.make_purchase(-5)
+product1.make_purchase(0)
